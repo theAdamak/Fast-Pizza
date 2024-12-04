cbea9938265a331f4b645e749fab8ff74bcb59df
mahdi ezati <mahdiezati80@gmail.com>
2024-12-05 01:40:25 +0330

    api and id added to order featurs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import MenuLoader from "./features/menu/MenuLoader";
-// import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+
+import CreateOrder from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
@@ -19,12 +19,17 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
-        MenuLoader,
+        loader: menuLoader,
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      // { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,16 +1,21 @@
 import { useLoaderData } from "react-router-dom";
-
+import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
-function Menu() {
+
+export default function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
+
   return (
     <ul>
       {menu.map((pizza) => (
-        <MenuItem key={pizza.id} pizza={pizza} />
+        <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
   );
 }
 
-export default Menu;
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
