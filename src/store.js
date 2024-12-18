a6418a69d35080dde84982d140b71dd532c71c17
mahdi ezati <mahdiezati80@gmail.com>
2024-12-18 22:54:59 +0330

    added several feature into logic.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReduser from "./features/user/userSlice";
+import cartReducer from "./features/cart/cartSlice";
 const store = configureStore({
   reducer: {
     user: userReduser,
+    cart: cartReducer,
   },
 });
 
